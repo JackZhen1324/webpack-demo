ede256117c8592f147aba000f03f23c099bf7446
ZhenQian <zqian15@asu.edu>
2021-06-24 15:52:43 +0800

    first commit

---
+++ .history/webpack.config_20210624143458.js
@@ -0,0 +1,62 @@
+module.exports = {
+    entry: './src/index.js',
+
+
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+
+                
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                loader: `url-loader`,
+                options: {
+                    limit: 8 * 1024,
+                    name: '[hash:10].[ext]',
+                    esModule:false
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: `html-loader`
+            },
+            {
+                // handle other resouces
+                exclude: /\.(html|js|css|less|jpg|gif)/,
+                loader: 'file-loader',
+                options: {
+                    name: '[hash:10].[ext]'
+                }
+            }
+
+
+        ]
+
+    },
+
+    Plugin: {
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+
+    }
+    devServer: {
+        content
+    }
+
+    
+
+
+}
